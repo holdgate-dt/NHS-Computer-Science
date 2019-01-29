89c37aed1fed9ddc976b06db8da37cbed2c5529c
holdgate-dt <47112857+holdgate-dt@users.noreply.github.com>
2019-01-29 10:47:45 -0500

    Update hello.py

--- HelloWorld/hello.py
+++ HelloWorld/hello.py
@@ -1,6 +1,12 @@
-from Tkinter import *
+from tkinter import *
 
+class Hello_GUI:
+  def __init__(self, master):
+    self.master = master
+    master.title("Hello World")
+
+    self.label = Label(master, text="Hello World!")
+    self.label.pack()
 root = Tk()
-theLabel = Label(root, text="Hello World")
-theLabel.pack()
+helloWorld = Hello_GUI(root)
 root.mainloop() 
